e2e3aa8edb57ba813844981d3547e8d3f51ac2f8
André de Souza Mendes <asmgma@gmail.com>
2021-07-05 15:24:32 -0300

    Add files via upload

---
+++ docs/DocVehicleSimpleLinear/DocVehicleSimpleLinear.m
@@ -0,0 +1,23 @@
+%% Vehicle Simple Linear
+% Linear bicycle model with 3 degrees of freedom.
+%
+% <html>
+% <script src='https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'></script>
+% </html>
+%
+%% Sintax
+% |dx = _VehicleModel_.Model(t,states,tspan)|
+%
+%% Arguments
+% The following table describes the input arguments:
+%
+% <html> <table border=1 width="97%">
+% <tr> <td width="30%"><tt>t</tt></td> <td width="70%">Time</td> </tr>
+% <tr> <td width="30%"><tt>states</tt></td> <td width="70%">Model state variables: [XT YT PSI VT ALPHAT dPSI]</td> </tr>
+% <tr> <td width="30%"><tt>tspan</tt></td> <td width="70%">Time span</td> </tr>
+% </table> </html>
+%
+%% See Also
+%
+% <../../../index.html Home>
+%
