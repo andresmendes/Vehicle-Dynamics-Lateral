397592c996c369cc00a119ce2c8ad135cd5efd78
André de Souza Mendes <asmgma@gmail.com>
2021-07-05 15:39:37 -0300

    Add files via upload

---
+++ docs/DocVehicleSimpleNonlinear4DOF/DocVehicleSimpleNonlinear4DOF.m
@@ -0,0 +1,26 @@
+%% Vehicle Simple Nonlinear 4DOF
+% Nonlinear bicycle model nonlinear with 4 degrees of freedom.
+%
+% <html>
+% <script src='https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'></script>
+% </html>
+%
+%
+%% Sintax
+% |dx = _VehicleModel_.Model(t,states,tspan)|
+%
+% |dx = _VehicleModel_.MassMatrix(t,states,tspan)|
+%
+%% Arguments
+% The following table describes the input arguments:
+%
+% <html> <table border=1 width="97%">
+% <tr> <td width="30%"><tt>t</tt></td> <td width="70%">Time</td> </tr>
+% <tr> <td width="30%"><tt>states</tt></td> <td width="70%">Model state variables: [XT YT PSI THETA VT ALPHAT dPSI dTHETA]</td> </tr>
+% <tr> <td width="30%"><tt>tspan</tt></td> <td width="70%">Time span</td> </tr>
+% </table> </html>
+%
+%% See Also
+%
+% <https://www.mathworks.com/matlabcentral/fileexchange/58683-vehicle-dynamics-lateral Home>
+%
